210d482416edef8379ba5de2bcdb8299fd83d92d
Nitin Jindal <nitinjindal335@gmail.com>
2026-03-25 05:17:57 -0700

    Create Arithmetic.cpp

---
+++ 04_Operators/Arithmetic.cpp
@@ -0,0 +1,13 @@
+// Topic: Operators
+// Program: To demonstrate arithmetic operators
+#include<iostream>
+using namespace std;
+int main() {
+int a = 10 , b = 5;
+cout<< "Addition: " << a + b << "\n"
+  << "Subtraction: " << a - b << "\n"
+  << "Multiplication: " << a * b << "\n"
+  << "Division: " << a / b << "\n"
+  << "Modulus: " << a % b << "\n";
+return 0;
+}
