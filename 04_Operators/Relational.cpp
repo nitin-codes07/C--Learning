d6dde29233d2794f5004995a4a400cb2d30da4b7
Nitin Jindal <nitinjindal335@gmail.com>
2026-03-27 22:34:08 -0700

    Create Relational.cpp

---
+++ 04_Operators/Relational.cpp
@@ -0,0 +1,12 @@
+// Topic: Operators
+// Program: To demonstrate relational operators
+#include<iostream>
+using namespace std;
+int main() {
+  int a = 10 , b = 20;
+cout << (a > b) << endl;
+cout << (a < b) << endl;
+cout << (a == b) << endl;
+cout << (a != b) << endl;
+return 0;
+}
