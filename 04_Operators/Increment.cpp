70e442aeb28a1b4958a329245dda01a59cb86520
Nitin Jindal <nitinjindal335@gmail.com>
2026-03-28 22:13:54 -0700

    Create Increment.cpp

---
+++ 04_Operators/Increment.cpp
@@ -0,0 +1,12 @@
+// Topic: Operators
+// Program: To demonstrate increment operators ( pre and post ) 
+#include<iostream>
+using namespace std;
+int main() {
+  int a = 5;
+cout << "Post Increment: " << a++ << endl;
+cout << "After Post Increment: " << a << endl;
+cout << "Pre Increment: " << ++a << endl;
+return 0;
+}
+  
