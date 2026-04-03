c0d4cb64879e0b63e0256a38cc732d0f80abd0f8
Nitin Jindal <nitinjindal335@gmail.com>
2026-04-02 22:40:44 -0700

    Create Greatest_Three.cpp

---
+++ 05_Flow_Control/Greatest_Three.cpp
@@ -0,0 +1,17 @@
+// Topic: Flow control
+// Program: To find the largest among three numbers
+#include<iostream>
+using namespace std;
+int main() {
+  int a , b , c;
+cout << " Enter three numbers: " ;
+cin >> a >> b >> c ;
+if ( a >= b && a >= c ) {
+cout << " Largest: " << a ;
+} else if ( b >=a && b >= c ) {
+cout << " Largest: " << b ;
+} else {
+cout << " Largest: " << c ;
+}
+return 0;
+}
